5b5d1b6b62fbf5cd2c4f663866c21551f215c3fa
Ruslan Kruhlov <zolruslan@ukr.net>
2016-12-01 14:58:10 +0200

    Created class

--- CKut.h
+++ CKut.h
@@ -1,4 +1,13 @@
 #ifndef EXAM_H
 #define EXAM_H
 
+class CKut {
+private:
+    int g;
+    friend bool Compare (CKut *kut1, CKut *kut2);
+public:
+    CKut(int g);
+    CKut(CKut *kut);
+};
+
 #endif
