45a20ccffe7a5fc70bd4bc813c914fa7178c910c
Ruslan Kruhlov <zolruslan@ukr.net>
2016-12-01 15:15:48 +0200

    Created case

--- main.cpp
+++ main.cpp
@@ -11,4 +11,24 @@ int main(void) {
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
+    delete kut1;
+    delete kut2;
+
+    kut1 = new CKut (55);
+    kut2 = new CKut (35);
+
+        if (Compare(kut1, kut2))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    delete kut1;
+    delete kut2;
+
+        kut1 = new CKut (90);
+    kut2 = new CKut (0);
+
+        if (Compare(kut1, kut2))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 }
